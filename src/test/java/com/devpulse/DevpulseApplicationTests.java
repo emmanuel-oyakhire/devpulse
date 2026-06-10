812b94127753b38d9eb7aa7a6c62d426bcb0ab77
emmanuel-oyakhire <eo.oyakhire@gmail.com>
2026-06-10 12:59:04 +0100

    test: add integration tests for registration and protected routes

--- src/test/java/com/devpulse/DevpulseApplicationTests.java
+++ src/test/java/com/devpulse/DevpulseApplicationTests.java
@@ -1,13 +1,73 @@
 package com.devpulse;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.jmx.support.ObjectNameManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Map;
+
+
+//import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("local")
 class DevpulseApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
 	@Test
-	void contextLoads() {
-	}
+	void registration_succeeds_with_valid_data() throws Exception{
+        Map<String, String> request = Map.of(
+                "name", "Test User",
+                "email", "testuser_" + System.currentTimeMillis() + "@devpulse.com",
+                "password", "testpassword123");
+
+        mockMvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.token").exists());
+    }
+
+    @Test
+    void registration_fails_with_duplication_email() throws Exception {
+        String email = "duplicate_" + System.currentTimeMillis();
+
+        Map<String, String> request = Map.of(
+                "name", "Test User",
+                "email", email,
+                "password", "testpassword123"
+        );
+
+        mockMvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error").value("Email already registered"));
+    }
+@Test
+    void protected_endpoint_reject_unauthenticated_request() throws Exception {
+        mockMvc.perform(get("/api/user/me"))
+                .andExpect(status().isForbidden());
+}
+
 
 }
